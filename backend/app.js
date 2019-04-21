ed16b896bb1451f2ce6c991db3c86e6139f38353
Oliver Brummel <oliverbrummel1@gmail.com>
2019-04-21 17:07:44 -0500

    Update endpoints, and add headers/bodyParser

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
-  console.log('first middleware')
-  console.log('req url', req.url)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('hello from express')
+app.post('/api/earrings', (req, res, next) => {
+  const newEarring = req.body;
+  console.log('New Earring Post!', newEarring);
+});
+
+app.use('/api/earrings', (req, res, next) => {
+  const jewelryBags = [
+    {
+      id: 1,
+      name: 'Golden Bulbs',
+      desc: 'oxidized silver, gold, gold leaf over wood',
+      image: ''
+    },
+    {
+      id: 2,
+      name: 'Big Red Hearts',
+      desc: 'enamel, silver, gold, black onyx',
+      image: ''
+    }
+  ];
+  res.status(200).json({
+    message: 'Earrings fetched successfully!',
+    data: jewelryBags
+  });
 });
 
 module.exports = app;
