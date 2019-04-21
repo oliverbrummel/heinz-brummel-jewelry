d92158fa699506edfedfc4cc6fcee3e0d4e00c19
Oliver Brummel <oliverbrummel1@gmail.com>
2019-04-21 17:06:05 -0500

    Import and inject httpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
   title = 'heinz-brummel-jewelry';
+
+  ngOnInit() {
+    //
+  }
 }
