00a12991ddc7289aad2278344a19a186e7296696
Oliver Brummel <oliverbrummel1@gmail.com>
2019-04-26 21:24:42 -0500

    Add thumbnail cloud function as instructed in video

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,61 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+import { Storage } from '@google-cloud/storage';
+const gcs = new Storage();
+
+import { tmpdir } from 'os';
+import { join, dirname } from 'path';
+
+import * as sharp from 'sharp'; // if this doesnt work, try 'import { sharp }...' syntax
+import * as fs from 'fs-extra'; // if this doesnt work, try 'import { fs }...' syntax
+
+export const generateThumbs = functions.storage
+  .object()
+  .onFinalize(async object => {
+    const bucket = gcs.bucket(object.bucket);
+    const filePath = object.name; // get full path to file in bucket
+    const fileName = filePath.split('/').pop();
+    const bucketDir = dirname(filePath);
+
+    const workingDir = join(tmpdir(), 'thumbs');
+    const tmpFilePath = join(workingDir, 'source.png'); // should this be jpg or anything?
+
+    // exit function if image has already been resized, or if there is no image in the upload
+    if (fileName.includes('thumb@') || !object.contentType.includes('image')) {
+      console.log('exiting function');
+      return false;
+    }
+
+    // 1. Ensure thumbnail dir exists
+    await fs.ensureDir(workingDir);
+
+    // 2. Download Source File
+    await bucket.file(filePath).download({
+      destination: tmpFilePath
+    });
+
+    // 3. Resize the images and define an array of upload promises
+    const sizes = [64, 128, 256];
+
+    const uploadPromises = sizes.map(async size => {
+      const thumbName = `thumb@${size}_${fileName}`;
+      const thumbPath = join(workingDir, thumbName);
+
+      // Resize source image
+      await sharp(tmpFilePath)
+        .resize(size, size)
+        .toFile(thumbPath);
+
+      // Upload to GCS
+      return bucket.upload(thumbPath, {
+        destination: join(bucketDir, thumbName)
+      });
+    });
+
+    // 4. Run the upload operations
+    await Promise.all(uploadPromises);
+
+    // 5. Cleanup remove the tmp/thumbs from the filesystem
+    return fs.remove(workingDir);
+  });
+
